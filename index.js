2250cfa0a6c123c59b08a8b35bf0a5f04bb2cce2
alquama salim <alquama00s@outlook.com>
2020-04-27 23:32:38 +0530

    improved nav for phone

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function showNav(){
-    alert("imal");
-    document.getElementsByClassName("header").style.backgroundColor="black";
+        if(document.getElementById("menu-btn").checked==true){
+        document.getElementsByClassName("header")[0].style.backgroundColor="rgb(17, 134, 230)";
+        document.getElementsByClassName("header")[0].style.transitionProperty="background-color, max-height";
+        document.getElementsByClassName("header")[0].style.transitionDuration="0.2s, 0.5s";
+        document.getElementsByClassName("header")[0].style.transionTimingFunction="ease-out";
+        document.getElementsByClassName("header")[0].style.maxHeight="100%";
+        document.getElementsByClassName("logo")[0].style.color="#fff";
+    }
+    else if(document.getElementById("menu-btn").checked==false){
+        document.getElementsByClassName("header")[0].style.backgroundColor="#fff";    
+        document.getElementsByClassName("header")[0].style.transitionProperty="background-color, max-height";
+        document.getElementsByClassName("header")[0].style.transitionDuration="2s, 0.5s";
+        document.getElementsByClassName("header")[0].style.transionTimingFunction="ease-out";
+        document.getElementsByClassName("header")[0].style.maxHeight="9%";
+        document.getElementsByClassName("logo")[0].style.color="black";
+        
+    }
 
 }
