dc21700d3ed2da7edef3db5025b73f1a1cacc073
alquama salim <alquama00s@outlook.com>
2020-04-28 10:10:07 +0530

    improved nav bar

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function showNav(){
         if(document.getElementById("menu-btn").checked==true){
         document.getElementsByClassName("header")[0].style.backgroundColor="rgb(17, 134, 230)";
+        document.getElementsByClassName("logo")[0].style.transition="background-color 0.2s ease-out";
+        document.getElementsByClassName("logo")[0].style.backgroundColor="rgb(17, 134, 230)";
         document.getElementsByClassName("header")[0].style.transitionProperty="background-color, max-height";
         document.getElementsByClassName("header")[0].style.transitionDuration="0.2s, 0.5s";
         document.getElementsByClassName("header")[0].style.transionTimingFunction="ease-out";
@@ -8,6 +10,8 @@ function showNav(){
         document.getElementsByClassName("logo")[0].style.color="#fff";
     }
     else if(document.getElementById("menu-btn").checked==false){
+        document.getElementsByClassName("logo")[0].style.backgroundColor="#fff";
+        document.getElementsByClassName("logo")[0].style.transition="background-color 2s ease-out";
         document.getElementsByClassName("header")[0].style.backgroundColor="#fff";    
         document.getElementsByClassName("header")[0].style.transitionProperty="background-color, max-height";
         document.getElementsByClassName("header")[0].style.transitionDuration="2s, 0.5s";
