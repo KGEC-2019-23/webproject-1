d7aec9aa1b76315aec9827065bc992b07e787572
alquama salim <alquama00s@outlook.com>
2020-04-28 13:26:18 +0530

    improved nav bar

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
+window.onload=function load(){
+    if(performance.navigation.type == 2){
+        document.getElementsByClassName("navMenu")[0].style.height="0";
+        window.location.reload(false);
+     }
+}
 function showNav(){
         if(document.getElementById("menu-btn").checked==true){
+            document.getElementsByClassName("navMenu")[0].style.height="100%";    
         document.getElementsByClassName("header")[0].style.backgroundColor="rgb(17, 134, 230)";
-        document.getElementsByClassName("logo")[0].style.transition="background-color 0.2s ease-out";
-        document.getElementsByClassName("logo")[0].style.backgroundColor="rgb(17, 134, 230)";
-        document.getElementsByClassName("header")[0].style.transitionProperty="background-color, max-height";
+        document.getElementsByClassName("header")[0].style.transitionProperty="background-color, height";
         document.getElementsByClassName("header")[0].style.transitionDuration="0.2s, 0.5s";
         document.getElementsByClassName("header")[0].style.transionTimingFunction="ease-out";
-        document.getElementsByClassName("header")[0].style.maxHeight="100%";
+        document.getElementsByClassName("header")[0].style.height="100%";
         document.getElementsByClassName("logo")[0].style.color="#fff";
     }
     else if(document.getElementById("menu-btn").checked==false){
-        document.getElementsByClassName("logo")[0].style.backgroundColor="#fff";
-        document.getElementsByClassName("logo")[0].style.transition="background-color 2s ease-out";
+        document.getElementsByClassName("navMenu")[0].style.height="0";    
         document.getElementsByClassName("header")[0].style.backgroundColor="#fff";    
-        document.getElementsByClassName("header")[0].style.transitionProperty="background-color, max-height";
-        document.getElementsByClassName("header")[0].style.transitionDuration="2s, 0.5s";
+        document.getElementsByClassName("header")[0].style.transitionProperty="background-color, height";
+        document.getElementsByClassName("header")[0].style.transitionDuration="0.8s, 0.5s";
         document.getElementsByClassName("header")[0].style.transionTimingFunction="ease-out";
-        document.getElementsByClassName("header")[0].style.maxHeight="auto";
+        document.getElementsByClassName("header")[0].style.height="3.5em";
         document.getElementsByClassName("logo")[0].style.color="black";
         
     }
